e9a6ea6e67c504556fb4759577ad2ef5ad29d35a
Style <esrsamarth@gmail.com>
2024-08-12 13:38:30 +0530

    dropping this porject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const app = () => {
+import './App.css'
+import { Footer, Blog, Features, Header, Possibility, WhatGPT3} from './containers';
+import {Cta, Brand, Navbar} from './components'
+
+const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/> 
+      </div>
+      <Brand/> 
+      <WhatGPT3/> 
+      <Features/> 
+      <Possibility/>
+      <Cta/>
+      <Blog/> 
+      <Footer/>   
     </div>
   )
 }
 
-export default app
+export default App
